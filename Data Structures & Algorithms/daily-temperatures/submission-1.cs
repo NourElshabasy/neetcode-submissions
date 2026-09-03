53de0504a1909833d8d079de3216f93ab8da36c0
Nour Elshabasy <156618454+NourElshabasy@users.noreply.github.com>
2026-09-03 16:56:00 -0400

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] DailyTemperatures(int[] temperatures) {
+        var stack = new Stack<int[]>();
+        var result = new int[temperatures.Length];
+
+        for(int i = 0; i < temperatures.Length; i++)
+        {
+            while(stack.Count > 0 && temperatures[i] > stack.Peek()[0])
+            {
+                var prev = stack.Pop();
+                int prevIndex = prev[1];
+                result[prevIndex] = i - prevIndex;
+            }
+            stack.Push(new int[] { temperatures[i], i });
+        }
+
+        return result;
+    }
+}
